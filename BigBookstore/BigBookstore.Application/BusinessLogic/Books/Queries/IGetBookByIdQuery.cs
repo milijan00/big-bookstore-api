f81342c197d6f5d1d6b8cd866eadb13afcdbf7b9
Aleksandar Milijanovic <acathebest00@gmail.com>
2023-02-05 11:37:44 +0100

    Books crud interfaces are included.

---
+++ BigBookstore/BigBookstore.Application/BusinessLogic/Books/Queries/IGetBookByIdQuery.cs
@@ -0,0 +1,13 @@
+﻿using BigBookstore.Application.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigBookstore.Application.BusinessLogic.Books.Queries
+{
+    public interface IGetBookByIdQuery : IQuery<BookDto>
+    {
+    }
+}
---
+++ BigBookstore/BigBookstore.Application/Dtos/BookDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigBookstore.Application.Dtos
+{
+    public class BookDto : BaseDto
+    {
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public string Letter { get; set; }
+        public string BindingType { get; set; }
+        public string Category { get; set; }
+        public uint Pages { get; set; }
+
+    }
+}
