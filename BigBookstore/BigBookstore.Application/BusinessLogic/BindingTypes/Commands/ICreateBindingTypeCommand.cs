e6cfe71e7f65c34b8ec5e22852b82210f54f9b48
Aleksandar Milijanovic <acathebest00@gmail.com>
2023-02-05 11:05:37 +0100

    BindingTypes crud interfaces are included.

---
+++ BigBookstore/BigBookstore.Application/BusinessLogic/BindingTypes/Commands/ICreateBindingTypeCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace BigBookstore.Application.BusinessLogic.BindingTypes.Commands
+{
+    public interface ICreateBindingTypeCommand : ICommand<Unit>
+    {
+    }
+}
