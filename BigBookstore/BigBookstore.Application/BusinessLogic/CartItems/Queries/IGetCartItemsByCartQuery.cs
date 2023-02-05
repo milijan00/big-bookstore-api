d02f775a127a2f8bf17c48fcffd35c7d13fee919
Aleksandar Milijanovic <acathebest00@gmail.com>
2023-02-05 12:23:13 +0100

    CartItem create, read and delete operations' interfaces are included.

---
+++ BigBookstore/BigBookstore.Application/BusinessLogic/CartItems/Queries/IGetCartItemsByCartQuery.cs
@@ -0,0 +1,13 @@
+﻿using BigBookstore.Application.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigBookstore.Application.BusinessLogic.CartItems.Queries
+{
+    public interface IGetCartItemsByCartQuery : IQuery<IEnumerable<CartItemDto>>
+    {
+    }
+}
---
+++ BigBookstore/BigBookstore.Application/Dtos/CartItemDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigBookstore.Application.Dtos
+{
+    public class CartItemDto
+    {
+        public Guid BookId { get; set; }
+        public Guid CartId { get; set; }
+    }
+}
