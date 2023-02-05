12fd61884c75232904928e7ed641e1f682e31e91
Aleksandar Milijanovic <acathebest00@gmail.com>
2023-02-05 12:13:40 +0100

    Carts crud interfaces are included.

---
+++ BigBookstore/BigBookstore.Application/BusinessLogic/Carts/Queries/IGetCartByUserIdQuery.cs
@@ -0,0 +1,13 @@
+﻿using BigBookstore.Application.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigBookstore.Application.BusinessLogic.Carts.Queries
+{
+    public interface IGetCartByUserIdQuery : IQuery<CartDto>
+    {
+    }
+}
---
+++ BigBookstore/BigBookstore.Application/Dtos/CartDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigBookstore.Application.Dtos
+{
+    public class CartDto : BaseDto
+    {
+        public Guid UserId { get; set; }
+        public string Address { get; set; }
+    }
+}
