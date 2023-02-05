e826fe950414cc2714479a05b1be845587c9b7be
Aleksandar Milijanovic <acathebest00@gmail.com>
2023-02-05 10:26:07 +0100

    Base interfaces are created and Role CRUD interaces are created as well.

---
+++ BigBookstore/BigBookstore.Application/BusinessLogic/IUseCaseHandler.cs
@@ -0,0 +1,15 @@
+﻿using BigBookstore.Application.Services;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigBookstore.Application.BusinessLogic
+{
+    public interface IUseCaseHandler<IUseCase, TResponse> : IRequestHandler<IUseCase<TResponse>, TResponse>
+    {
+        public IApplicationService ApplicationService { get; set; }
+    }
+}
---
+++ BigBookstore/BigBookstore.Application/Services/IApplicationService.cs
@@ -0,0 +1,19 @@
+﻿using BigBookstore.Persistance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigBookstore.Application.Services
+{
+    public interface IApplicationService
+    {
+        public  Task<TEntity> GetByIdAsync<TEntity>( Guid id);
+        public Task<IEnumerable<TEntity>> GetAsync<TEntity>();
+        public Task CreateAsync<TEntity>(TEntity entity);
+        // ToDo: Think of better solution for this update method
+        public Task UpdateAsync<TEntity>(TEntity entity, Guid id);
+        public Task DeleteAsync<TEntity>(Guid id);
+    }
+}
