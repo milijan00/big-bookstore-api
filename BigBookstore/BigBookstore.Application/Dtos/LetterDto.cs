8e42e6c1fd31f2bb4539835d62d2c6086c8a6e38
Aleksandar Milijanovic <acathebest00@gmail.com>
2023-02-05 11:24:17 +0100

    Letters crud interfaces are included.

---
+++ BigBookstore/BigBookstore.Application/Dtos/LetterDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigBookstore.Application.Dtos
+{
+    public class LetterDto : BaseDto
+    {
+        public string Name { get; set; }
+    }
+}
