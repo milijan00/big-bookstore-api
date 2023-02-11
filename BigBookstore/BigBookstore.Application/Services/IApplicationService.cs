d1de087c0ca42f384e8203f64ba3976ccaf54b19
Aleksandar Milijanovic <acathebest00@gmail.com>
2023-02-11 10:18:08 +0100

    IApplicationService is refactored and Entity method is added.

--- BigBookstore/BigBookstore.Application/Services/IApplicationService.cs
+++ BigBookstore/BigBookstore.Application/Services/IApplicationService.cs
@@ -1,4 +1,6 @@
-﻿using BigBookstore.Persistance;
+﻿using BigBookstore.Application.Dtos;
+using BigBookstore.Persistance;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,11 +11,11 @@ namespace BigBookstore.Application.Services
 {
     public interface IApplicationService
     {
-        public  Task<TEntity> GetByIdAsync<TEntity>( Guid id);
-        public Task<IEnumerable<TEntity>> GetAsync<TEntity>();
-        public Task CreateAsync<TEntity>(TEntity entity);
-        // ToDo: Think of better solution for this update method
-        public Task UpdateAsync<TEntity>(TEntity entity, Guid id);
+        public  Task<TEntity> GetByIdAsync<TEntity>( Guid id) where TEntity : class ;
+        public Task<IEnumerable<TEntity>> GetAsync<TEntity>() where TEntity : class;
+        public Task CreateAsync<TEntity>(TEntity entity) where TEntity : class;
+        public Task UpdateAsync<TEntity, TDto>( Guid id, TDto entity) where TDto : BaseDto where TEntity : class;
         public Task DeleteAsync<TEntity>(Guid id);
+        public Task<DbSet<TEntity>> Entity<TEntity>() where TEntity: class;
     }
 }
