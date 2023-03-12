fb03c7aa4a163e4b7b530502122d0f0e1dc61598
Aleksandar Milijanovic <acathebest00@gmail.com>
2023-03-12 15:37:07 +0100

    Logger service adde.

---
+++ BigBookstore/BigBookstore.Application/Services/ILoggerService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigBookstore.Application.Services
+{
+    public interface ILoggerService 
+    {
+        Task Log(string message, string stackTrace);
+    }
+}
---
+++ BigBookstore/BigBookstore.Implementation/Services/LoggerService.cs
@@ -0,0 +1,35 @@
+﻿using BigBookstore.Application.Services;
+using BigBookstore.Domain.Entities;
+using BigBookstore.Persistance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigBookstore.Implementation.Services
+{
+    public class LoggerService : ILoggerService
+    {
+        private readonly ApplicationService service;
+
+        public LoggerService(ApplicationService service)
+        {
+            this.service = service;
+        }
+        public async  Task Log(string message, string stackTrace)
+        {
+            if (string.IsNullOrEmpty(message))
+            {
+                throw new ArgumentException("Exceptions's message must not be empty.");
+            }
+            await this.service.Entity<LoggedException>().AddAsync(new LoggedException
+            {
+                Id = Guid.NewGuid(),
+                Message = message,
+                StackTrace = stackTrace
+            });
+            await this.service.SaveChangesAsync();
+        }
+    }
+}
